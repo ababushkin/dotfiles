fedaac8972fd7503ec5ceecaf2d34aac2b3172ae
Anton Babushkin <vegitto.ru@gmail.com>
2021-07-11 19:31:00 +1000

    Fish config

---
+++ config.fish
@@ -0,0 +1,8 @@
+set -x EDITOR nvim
+set -x TERM screen-256color
+set -x LANG en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+set -x PATH $PATH ~/Library/Python/3.9/bin
+
+alias vim nvim
